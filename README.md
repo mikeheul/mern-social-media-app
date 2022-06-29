d2fee73a326c2fcb12af58d7087919ded67f337f
Mikeheul <mickael.murmann@gmail.com>
2022-06-29 23:10:17 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# mern-social-media
+
+![MongoDB](https://img.shields.io/static/v1?style=for-the-badge&message=MongoDB&color=47A248&logo=MongoDB&logoColor=FFFFFF&label=)
+![Express](https://img.shields.io/static/v1?style=for-the-badge&message=Express&color=000000&logo=Express&logoColor=FFFFFF&label=)
+![React](https://img.shields.io/static/v1?style=for-the-badge&message=React&color=222222&logo=React&logoColor=61DAFB&label=)
+![Node.js](https://img.shields.io/static/v1?style=for-the-badge&message=Node.js&color=339933&logo=Node.js&logoColor=FFFFFF&label=)
+![Nodemon](https://img.shields.io/static/v1?style=for-the-badge&message=Nodemon&color=222222&logo=Nodemon&logoColor=76D04B&label=)
+![npm](https://img.shields.io/static/v1?style=for-the-badge&message=npm&color=CB3837&logo=npm&logoColor=FFFFFF&label=)
+![Sass](https://img.shields.io/static/v1?style=for-the-badge&message=Sass&color=CC6699&logo=Sass&logoColor=FFFFFF&label=)
---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+//* MIDDLEWARES
+// Controls the maximum request body size
+app.use(bodyParser.json( {limit: '30mb', extended: true}));
+// Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option.
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+// CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://mikeheul:mikeheul123@cluster0.vlqbc.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { 
+    useUnifiedTopology: true })
+        .then(() => app.listen(PORT, () => console.log(`Listening on Server ${PORT}`)))
+        .catch((error) => console.log(error.message))
+
+
+
+
+
